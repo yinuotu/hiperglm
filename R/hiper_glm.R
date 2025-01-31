182008ec15d8fbfe290f27c7004ece26706c7f8e
yinuotu <179552534+yinuotu@users.noreply.github.com>
2025-01-31 14:49:04 -0500

    Set up model_output.R and hiper_glm.R

---
+++ R/hiper_glm.R
@@ -0,0 +1,6 @@
+hiper_glm <- function(design, outcome) {
+  warning("This function is to be implemented")
+  hglm_out <- list()
+  class(hglm_out) <- "hglm" # Make it S3
+  return(hglm_out)
+}
---
+++ R/model_output.R
@@ -0,0 +1,11 @@
+coef.hglm <- function(hglm_out) {
+  warning("This function is yet to be implemented.")
+}
+
+vcov.hglm <- function(hglm_out) {
+  warning("This function is yet to be implemented.")
+}
+
+print.hglm <- function(hglm_out) {
+  cat("Output of hiper_glm.\n")
+}
